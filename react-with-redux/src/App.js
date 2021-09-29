14c4702d06a6076f1a5cfee925517f8884545e87
Isaac Bruno <isaacwinfor@gmail.com>
2021-09-29 18:06:12 -0300

    Action, Action Creators, and useDispatch

--- react-with-redux/src/App.js
+++ react-with-redux/src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from './state';
 
 function App() {
   const account = useSelector((state) => state.account);
+  const dispatch = useDispatch();
 
-  console.log(account);
+  const { depositMoney, withdrawMoney } = bindActionCreators(actionCreators, dispatch);
 
   return (
     <div className="App">
+      <h1>{account}</h1>
+      <button onClick={() => depositMoney(1000)}>Deposit</button>
+      <button onClick={() => withdrawMoney(1000)}>Withdraw</button>
     </div>
   );
 }
--- react-with-redux/src/state/store.js
+++ react-with-redux/src/state/store.js
@@ -1,7 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
+import thunk from 'redux-thunk';
 
 export const store = createStore(
   reducers,
-  {}
+  {},
+  applyMiddleware(thunk)
 );
