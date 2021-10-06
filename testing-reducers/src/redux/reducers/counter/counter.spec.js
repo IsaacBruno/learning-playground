3170a707d2f7323db45db189a50a2c5ebdd88a63
Isaac Bruno <isaacwinfor@gmail.com>
2021-10-06 16:44:30 -0300

    test: ensure counterReducer handles INCREMENT

--- testing-reducers/src/redux/reducers/counter/counter.spec.js
+++ testing-reducers/src/redux/reducers/counter/counter.spec.js
@@ -5,4 +5,9 @@ describe('counter reducer', () => {
     const result = counterReducer(undefined, {});
     expect(result).toBe(0);
   });
+
+  test('should handle INCREMENT', () => {
+    const result = counterReducer(0, { type: 'INCREMENT' });
+    expect(result).toBe(1);
+  });
 });
