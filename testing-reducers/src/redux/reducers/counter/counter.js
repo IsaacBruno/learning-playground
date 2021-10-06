2a860d159894d4e9a589f15bd69133e90b1bb463
Isaac Bruno <isaacwinfor@gmail.com>
2021-10-06 16:51:57 -0300

    feat: ensure counterReducer handles DECREMENT

--- testing-reducers/src/redux/reducers/counter/counter.js
+++ testing-reducers/src/redux/reducers/counter/counter.js
@@ -2,6 +2,8 @@ const reducer = (state=0, action) => {
   switch (action.type) {
     case 'INCREMENT':
       return state + 1;
+    case 'DECREMENT':
+      return state - 1;
     default:
       return state;
   }
